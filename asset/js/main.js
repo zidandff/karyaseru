2a85570b01881137f7e2110a98945fdd4bb88af7
Zidan M. Daffa <zidandaff@gmail.com>
2021-01-30 21:32:05 +0700

    Upload File

---
+++ asset/js/main.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector(".navbar")
+
+document.addEventListener('scroll', ()=> {
+    if( pageYOffset > 80 ){
+        navbar.classList.add("scroll")
+    }else {
+        navbar.classList.remove("scroll")
+    }
+})
+
+document.querySelector(".navbar-toggler").addEventListener('click', ()=> {
+    navbar.classList.add("scroll")
+})
+
+// AOS Initialization
+AOS.init({
+    offset: 120,
+    mirror: true,
+    duration: 1000,
+    easing: 'ease-in-out-back'
+});
+
+// Smooth scroll initialization
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800,
+    easing: 'easeInQuad',
+    offset: 120
+});
